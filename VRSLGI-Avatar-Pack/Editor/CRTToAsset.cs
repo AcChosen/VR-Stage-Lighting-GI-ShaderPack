266cab65c7a05990cd8733f832ff84582eb7ef5b
AcChosen <nexaka2@gmail.com>
2023-10-05 10:26:43 -0400

    Added Avatar Testing Pack

---
+++ VRSLGI-Avatar-Pack/Editor/CRTToAsset.cs
@@ -0,0 +1,75 @@
+﻿using UnityEditor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+
+public class CRTToAsset : EditorWindow
+{
+    string m_ScriptFilePath;
+    string m_ScriptFolder;
+    string path = "Assets/VRSL Addons/VRSL-GI Shader Package/VRSLGI-Avatar-Pack"; 
+    string filename;
+    public RenderTexture source;
+    [MenuItem("Tools/RT To Asset")]
+        static void Init()
+    {
+        EditorWindow window = GetWindowWithRect<CRTToAsset>(new Rect(0, 0, 600, 200));
+        window.titleContent = new GUIContent("RT To Asset File");
+        window.Show();
+    }
+
+    void ReadSetupData()
+    {
+        MonoScript ms = MonoScript.FromScriptableObject( this );
+        m_ScriptFilePath = AssetDatabase.GetAssetPath( ms );
+
+        FileInfo fi = new FileInfo( m_ScriptFilePath);
+        m_ScriptFolder = fi.Directory.ToString();
+        m_ScriptFolder.Replace( '\\', '/');
+
+        //Debug.Log( m_ScriptFolder );
+    }
+    Texture2D toTexture2D(RenderTexture rTex)
+    {
+        Texture2D tex = new Texture2D(rTex.width, rTex.height, TextureFormat.RGBAFloat, false);
+        Debug.Log("Copying Render Texture");
+        // ReadPixels looks at the active RenderTexture.
+        RenderTexture.active = rTex;
+        tex.ReadPixels(new Rect(0, 0, rTex.width, rTex.height), 0, 0);
+        tex.Apply();
+        return tex;
+    }
+    void OnGUI()
+    {
+        EditorGUILayout.BeginHorizontal();
+        source = (RenderTexture )EditorGUILayout.ObjectField(source, typeof(RenderTexture), true);
+        EditorGUILayout.EndHorizontal();
+        path = EditorGUILayout.TextField(new GUIContent("Path"),path);
+        filename = EditorGUILayout.TextField(new GUIContent("Name"),filename);
+        if (GUILayout.Button("Export Render Texture"))
+        {
+            try{
+            if(source is RenderTexture)
+            {
+                // RenderTexture extraTex = new RenderTexture(source.width, source.height, source.depth, source.format);
+                // extraTex.Create();
+                // Graphics.Blit(source, extraTex);
+                Texture2D myTexture = toTexture2D(source);
+                myTexture.filterMode = FilterMode.Point;
+                AssetDatabase.CreateAsset(myTexture, path + "/" + filename +  ".asset");
+            }
+            }
+            catch(Exception e)
+            {
+                Debug.LogError("Message: " + e.Message.ToString());
+                Debug.LogError("StackTrace: " + e.StackTrace.ToString());
+            }
+        }
+    }
+
+
+
+}
---
+++ VRSLGI-Avatar-Pack/SetGlobalTexture.cs
@@ -0,0 +1,62 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class SetGlobalTexture : MonoBehaviour
+{
+    public Texture2D[] targetTextures;
+    public Texture2D offTexture2D;
+    int selectedTexture;
+    
+
+    public void _SetGlobalTexture(int index)
+    {
+        if(targetTextures[index])
+        {
+            int textureID = Shader.PropertyToID("_Udon_VRSL_GI_LightTexture");
+            Shader.SetGlobalTexture(textureID, targetTextures[index]);
+            selectedTexture = index;
+        }
+    }
+
+    public void _ClearGlobalTexture()
+    {
+        if(offTexture2D)
+        {
+            int textureID = Shader.PropertyToID("_Udon_VRSL_GI_LightTexture");
+            Shader.SetGlobalTexture(textureID, offTexture2D);
+        }
+    }
+    void Start()
+    {
+        _SetGlobalTexture(selectedTexture);
+    }
+}
+[CustomEditor(typeof(SetGlobalTexture))]
+public class SetGlobalTexture_Editor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        SetGlobalTexture setGlobalTexture = target as SetGlobalTexture;
+        for(int i = 0; i < setGlobalTexture.targetTextures.Length; i++)
+        {
+            if(GUILayout.Button("Set Texture: " + setGlobalTexture.targetTextures[i].name))
+            {
+                setGlobalTexture._SetGlobalTexture(i);
+            }  
+        } 
+        if(GUILayout.Button("Clear VRSL GI Texture"))
+        {
+            setGlobalTexture._ClearGlobalTexture();
+        }   
+        DrawDefaultInspector();
+
+    }
+}
+#endif
